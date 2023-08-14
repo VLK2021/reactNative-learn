c913cd64243849d8932580c01742bc5e0a50ddfe
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-08-15 02:55:12 +0300

    commit

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {getUsers} from "./src/api/API";
 
 
 export default function App() {
+    const [users, setUsers] = useState([]);
+
+    console.log(users);
+
+    async function fetchData() {
+        let users = await getUsers();
+        setUsers([...users]);
+    }
+
+    useEffect(() => {
+        fetchData();
+
+        return ()=>{
+            console.log('hello');
+        }
+    }, []);
+
+
     return (
         <View>
 
@@ -10,6 +29,4 @@ export default function App() {
     );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
