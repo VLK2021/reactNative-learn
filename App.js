3d53d376aa94093b99ef87953b43eea7e15ec08c
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-08-15 02:06:53 +0300

    commit

--- App.js
+++ App.js
@@ -24,7 +24,6 @@ export default function App() {
                 data={users}
                 renderItem={({item}) => {
                     return <View>
-
                         <Text>
                             {item.name} - {item.age}
                         </Text>
